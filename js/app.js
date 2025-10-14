87aab70b1016c7d0c25d98a063bc869ecfbb5aee
Arthur Rocha <rocha.arthur.gon@gmail.com>
2025-10-14 16:45:29 -0300

    subtotal certo

--- js/app.js
+++ js/app.js
@@ -1,23 +1,16 @@
-function adicionar (){
-//recuperar valores, nome do produto, quantidade e valor
-let produto = document. getElementById('produto').value;
-let nomeProduto = produto.split('-')[0];
-let valorUnitario = produto.split('R$')[1];
-let quantidade = document.getElementById('quantidade').value;
-
-//calcurar o preeço, nosso subtotal.
-let preco = quantidade * valorUnitario;
-
-//adicionar no carinhos
-let carrinho = document.getElementById('lista-produtos')
-
-carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$ ')[1]; 
+    let quantidade = document.getElementById('quantidade').value; 
+        //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    //adicionar no carrinho
+        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
   </section>`;
-  
-let valorTotal = document.getElementById('valor-total');
-valorTotal = valorTotal + preco;
-valorTotal.innerHMTL = `${valorTotal}`;
 //atualizar o valor total 
 
 }
