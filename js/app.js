105f66946c7ba2d1dc70afa7c1a63887009f118b
Arthur Rocha <rocha.arthur.gon@gmail.com>
2025-10-15 15:28:29 -0300

    commit estranho

--- js/app.js
+++ js/app.js
@@ -11,10 +11,3 @@ function adicionar() {
         carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
   </section>`;
-//atualizar o valor total 
-
-}
-
-function limpar (){
-
-}
